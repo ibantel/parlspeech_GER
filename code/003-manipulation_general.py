bfa56876fce0791ca63000dd297174bffc638d3a
Ivo Bantel <42738203+ibantel@users.noreply.github.com>
2022-09-05 16:50:50 +0200

    Update 003-manipulation_general.py

--- code/003-manipulation_general.py
+++ code/003-manipulation_general.py
@@ -29,7 +29,49 @@ bt_long: pd.DataFrame =\
 
 del bt_file
 
+# %%
 
-# %% Code out
+dict_agenda_cleaning: dict = {
+    "Tagesordnungspunkt(e)?\s*\d*\s*((?![u])[a-z])?\s*(und|bis)?\s*\d*\s*((?![u])[a-z])?\s*:\s*": "",
+     # matches:
+     # Tagesordnungspunkt 23: l
+     # Tagesordnungspunkt 23:l
+     # Tagesordnungspunkt23: l
+     # Tagesordnungspunkt: l
+     # Tagesordnungspunkte 23 und 24: l
+     # Tagesordnungspunkt 21a und 21b:l
+     # Tagesordnungspunkt 21a und 21b: l
+     # Tagesordnungspunkte 21 a und 21 b:
+     # Tagesordnungspunkt 23: l
+     # Tagesordnungspunkt 23: l
+     # Tagesordnungspunkt 23: l
+     # Tagesordnungspunkt 23: l
+     # Tagesordnungspunkt 23: l
+     # Tagesordnungspunkt 1: l
+     # Tagesordnungspunkt 7 a bis 7 d: l
+    "Beratung\s*(des|der)\s*(Antrags|Beschlussempfehlung)\s*(und)?\s*(des)?\s*(Bericht(s)?)?\s*(des|der)?\s*(Ausschuss(es)?\s*für\s*)?": "",
+    # matches:
+    # Beratung der Beschlussempfehlung und des Berichts des
+    # Beratung des Antrags der
+    # Beratung der Beschlussempfehlung und des Berichts des Ausschusses für
+    "Dr\.\s*" : "",
+    "Abgeordneten\s*(.+)\s*weiterer\s*Abgeordneter\s*und\s*der\s*Fraktion\s*(der)?\s*(CDU\/CSU|SPD|FDP|AfD|DIE\s*LINKE|Die\s*Linke|BÜNDNIS\s*90\/DIE\s*GRÜNEN|Bündnis\s*90\/Die\s*Grünen)?\s*" : "",
+    # matches "Abgeordneten\s*[anything]\s*weiterer" (usw.)
+    # "1) Anlage 7  "
+    # "U+00AD"
+
+     }
+
+# Drucksachen
+# Antwort
+# Bundesregierung
+
+
+lst = bt_long['agenda'].replace(dict_agenda_cleaning, regex=True).unique().tolist()
+
+for i in lst:
+    if i.startswith("Beratung"):
+        print(i, end='\n\n')
+
 
 # analyse agenda points to different topics
